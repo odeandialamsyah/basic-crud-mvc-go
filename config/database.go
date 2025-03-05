4dfc7ef002358236b1d71982ddb4abfee62ae07b
odeandialamsyah <odeandialamsyah@gmail.com>
2025-03-05 22:10:29 +0700

    models and db connection

--- config/database.go
+++ config/database.go
@@ -12,9 +12,9 @@ var DB *gorm.DB
 func ConnectDB(){
 	dsn := "root:@tcp(127.0.0.1:3306)/crud_go?charset=utf8mb4&parseTime=True&loc=Local"
 	var err error
-	DB, err = gorm.Open(mysql.Open(dsn), $gorm.Config{})
+	DB, err = gorm.Open(mysql.Open(dsn), &gorm.Config{})
 	if err != nil {
-		fmt.Println("Error Connecting to Database:" err)
+		fmt.Println("Error Connecting to Database:", err)
 		panic(err)
 	}
 	fmt.Println("Database Connected Successfully!")
--- models/user.go
+++ models/user.go
@@ -1,1 +1,13 @@
 package models
+
+import "gorm.io/gorm"
+
+type User struct {
+	gorm.Model
+	Name string `json:"name"`
+	Email string `json:"email"`
+}
+
+func MigrateUsers(db *gorm.DB){
+	db.AutoMigrate(&User{})
+}
