a7e1ec75622e4c0378a93b79c8525d80d7be3ace
odeandialamsyah <odeandialamsyah@gmail.com>
2025-03-05 22:04:42 +0700

    database connection

--- config/database.go
+++ config/database.go
@@ -1,2 +1,21 @@
 package config
 
+import (
+	"fmt"
+
+	"gorm.io/driver/mysql"
+	"gorm.io/gorm"
+)
+
+var DB *gorm.DB
+
+func ConnectDB(){
+	dsn := "root:@tcp(127.0.0.1:3306)/crud_go?charset=utf8mb4&parseTime=True&loc=Local"
+	var err error
+	DB, err = gorm.Open(mysql.Open(dsn), $gorm.Config{})
+	if err != nil {
+		fmt.Println("Error Connecting to Database:" err)
+		panic(err)
+	}
+	fmt.Println("Database Connected Successfully!")
+}
