2fdd7094409ec51c805424e3230c5a1a5014d00e
odeandialamsyah <odeandialamsyah@gmail.com>
2025-03-05 22:28:03 +0700

    repository untuk query

--- repository/user.go
+++ repository/user.go
@@ -1,1 +1,39 @@
 package repository
+
+import (
+	"crud-mvc-go/models"
+
+	"gorm.io/gorm"
+)
+
+type UserRepository struct {
+	DB *gorm.DB
+}
+
+func NewUserRepository(db *gorm.DB) *UserRepository{
+	return &UserRepository{DB: db}
+}
+
+func (r *UserRepository) GetAllUsers() ([]models.User, error){
+	var users []models.User
+	err := r.DB.Find(&users).Error
+	return users, err
+}
+
+func (r *UserRepository) GetUserByID(id uint) (models.User, error){
+	var user models.User
+	err := r.DB.First(&user, id).Error
+	return user, err
+}
+
+func (r *UserRepository) CreateUser(user *models.User) error{
+	return r.DB.Create(user).Error
+}
+
+func (r *UserRepository) UpdateUser(user *models.User) error{
+	return r.DB.Save(user).Error
+}
+
+func (r *UserRepository) DeleteUser(id uint) error{
+	return r.DB.Delete(&models.User{}, id).Error
+}
