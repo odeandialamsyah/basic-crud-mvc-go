9819fe8b60929665f936fbee5ae71da454d7779f
odeandialamsyah <odeandialamsyah@gmail.com>
2025-03-07 06:54:38 +0700

    update models

--- models/user.go
+++ models/user.go
@@ -1,11 +1,18 @@
 package models
 
-import "gorm.io/gorm"
+import (
+	"time"
+
+	"gorm.io/gorm"
+)
 
 type User struct {
-	gorm.Model
-	Name string `json:"name"`
-	Email string `json:"email"`
+	IDUser uint   `gorm:"primaryKey;column:id_users" json:"id_users"`
+    Name  string `json:"name"`
+    Email string `json:"email"`
+    CreatedAt time.Time `json:"created_at"`
+    UpdatedAt time.Time `json:"updated_at"`
+    DeletedAt gorm.DeletedAt `json:"deleted_at"`
 }
 
 func MigrateUsers(db *gorm.DB){
